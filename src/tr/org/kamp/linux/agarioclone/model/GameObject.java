53cc359cbeff4c44ee361a2fc507d3e37a356fe8
Adilcan Eren <adilcan@kodgemisi.com>
2017-08-04 10:23:10 +0300

    adds documentation

--- src/tr/org/kamp/linux/agarioclone/model/GameObject.java
+++ src/tr/org/kamp/linux/agarioclone/model/GameObject.java
@@ -1,9 +1,17 @@
 package tr.org.kamp.linux.agarioclone.model;
 
 import java.awt.Color;
+
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
+/**
+ * 
+ * @author tansel
+ * @version 1.0
+ * A generic abstract class that can produce an enemy, a mine, a chip, a player.
+ *
+ */
 public abstract class GameObject {
 
 	private int x;
@@ -11,6 +19,14 @@ public abstract class GameObject {
 	private int radius;
 	private Color color;
 	
+	/**
+	 * 
+	 * @param x x coordinate
+	 * @param y y coordinate
+	 * @param radius
+	 * @param color
+	 * 
+	 */
 	public GameObject(int x, int y, int radius, Color color) {
 		this.x = x;
 		this.y = y;
@@ -18,11 +34,19 @@ public abstract class GameObject {
 		this.color = color;
 	}
 	
+	/**
+	 * A drawing method to draw with setting color and filling via fields.
+	 * @param g2d Using for setting color and filling oval.
+	 */
 	public void draw(Graphics2D g2d) {
 		g2d.setColor(getColor());
 		g2d.fillOval(getX(), getY(), getRadius(), getRadius());
 	}
 	
+	/**
+	 * Gets rectangle to scale collision
+	 * @return A rectangle object with fields x, y, radius.
+	 */
 	public Rectangle getRectangle() {
 		Rectangle rect = new Rectangle(getX(), getY(), getRadius(), getRadius());
 		return rect;
